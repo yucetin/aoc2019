8ad9d54581fb9add91b65bef53df478672fb23b3
kemal yucetin <kemalyucetin@gmail.com>
2019-12-09 18:33:54 +0000

    added data

--- lib/day2/02.exs
+++ lib/day2/02.exs
@@ -1,7 +1,29 @@
 defmodule Day02 do
   def run() do
     read_source()
-    # |> calc()
+    |> take4
+  end
+
+  defp take4(list) do
+    list
+
+    Enum.take(4)
+    |> process4(list)
+    |> IO.inspect()
+
+    take4(list)
+  end
+
+  defp process4([99, _, _, _], list), do: IO.puts(list)
+
+  defp process4([1, first, second, third], list) do
+    a = list[first] + list[second]
+    list[third] = a
+  end
+
+  defp process4([2, first, second, third], list) do
+    a = list[first] * list[second]
+    list[third] = a
   end
 
   defp read_source() do
