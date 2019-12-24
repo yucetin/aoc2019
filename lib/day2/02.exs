2ea4fcb85ad980ad6fadd1744439c5caf8c30916
kemal yucetin <kemalyucetin@gmail.com>
2019-12-24 01:23:24 +0000

    day two

--- lib/day2/02.exs
+++ lib/day2/02.exs
@@ -1,34 +1,39 @@
 defmodule Day02 do
   def run() do
-    read_source()
-    |> take4
-  end
+    origin_list = read_source()
 
-  defp take4(list) do
-    list
-
-    Enum.take(4)
-    |> process4(list)
+    ops(origin_list, origin_list)
     |> IO.inspect()
-
-    take4(list)
   end
 
-  defp process4([99, _, _, _], list), do: IO.puts(list)
+  defp ops([1 | [f | [s | [t | rest]]]], altered_list) do
+    altered_list =
+      List.update_at(altered_list, t, fn _ ->
+        Enum.at(altered_list, f) + Enum.at(altered_list, s)
+      end)
 
-  defp process4([1, first, second, third], list) do
-    a = list[first] + list[second]
-    list[third] = a
+    ops(rest, altered_list)
   end
 
-  defp process4([2, first, second, third], list) do
-    a = list[first] * list[second]
-    list[third] = a
+  defp ops([2 | [f | [s | [t | rest]]]], altered_list) do
+    altered_list =
+      List.update_at(altered_list, t, fn _ ->
+        Enum.at(altered_list, f) * Enum.at(altered_list, s)
+      end)
+
+    ops(rest, altered_list)
+  end
+
+  defp ops([99 | [f | [s | [t | rest]]]], altered_list) do
+    altered_list
   end
 
   defp read_source() do
     {:ok, source} = File.read("02.txt")
 
-    String.split(source, ",") |> IO.inspect() |> Enum.map(&String.to_integer/1)
+    String.trim(source, "\n")
+    |> String.split(",")
+    |> IO.inspect()
+    |> Enum.map(&String.to_integer/1)
   end
 end
